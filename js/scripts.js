8ab78f921d73611aceda81ab5f06d80280a7b83b
Josh Casuccio and Ryan Apking <josh@>
2016-08-30 13:21:11 -0700

    remove obsolete Board prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function Board() {
 };
 
 Board.prototype.buildBoard = function() {
-  for (i = 0; i < 20; i++) {
+  for (var i = 0; i < 20; i++) {
     var row = [];
     for(j = 0; j < 10; j++) {
       row.push("O");
@@ -21,7 +21,7 @@ Board.prototype.buildBoard = function() {
 Board.prototype.removeRow = function(row) {
   this.rows.splice(row, 1);
   var newRow = [];
-  for (i = 0; i < 10; i++) {
+  for (var i = 0; i < 10; i++) {
     newRow.push("O");
   }
   this.rows.unshift(newRow);
@@ -31,7 +31,7 @@ Board.prototype.removeRow = function(row) {
 
 Board.prototype.findFullRows = function() {
   var linesToClear = 0;
-  for (i = 0; i < 20; i++) {
+  for (var i = 0; i < 20; i++) {
     var test = true;
     for (j = 0; j < 10; j++) {
       if (this.rows[i][j] === "O") {
@@ -72,20 +72,6 @@ Board.prototype.lowerCurrentPiece = function() {
   }
 };
 
-Board.prototype.isBottomClear = function() {
-  for (i = 0; i < this.currentPiece.bottom.length; i++) {
-    var row = this.currentPiece.location[0] + this.currentPiece.bottom[i][0] + 1;
-    var column = this.currentPiece.location[1] + this.currentPiece.bottom[i][1];
-    // console.log(row);
-    if (row > 19) {
-      return false;
-    } else if (row >= 0 && this.rows[row][column] !== "O") {
-      return false;
-    }
-  }
-  return true;
-};
-
 Board.prototype.rightCurrentPiece = function() {
   var newLocation = [this.currentPiece.location[0], this.currentPiece.location[1] + 1];
   if (this.checkSpace(newLocation)) {
@@ -94,21 +80,6 @@ Board.prototype.rightCurrentPiece = function() {
     this.populateCurrentPiece();
   }
 };
-
-Board.prototype.isRightClear = function() {
-  // console.log(this.right);
-  for (i = 0; i < this.currentPiece.right.length; i++) {
-    var row = this.currentPiece.location[0] + this.currentPiece.right[i][0];
-    var column = this.currentPiece.location[1] + this.currentPiece.right[i][1] + 1;
-    // console.log(column);
-    if (column > 9) {
-      return false;
-    } else if (!row < 0 && this.rows[row][column] !== "O") {
-      return false;
-    }
-  }
-  return true;
-}
 
 Board.prototype.leftCurrentPiece = function() {
   var newLocation = [this.currentPiece.location[0], this.currentPiece.location[1] - 1];
@@ -122,19 +93,6 @@ Board.prototype.leftCurrentPiece = function() {
   //   this.currentPiece.location[1]--;
   //   this.populateCurrentPiece();
   // }
-};
-
-Board.prototype.isLeftClear = function() {
-  for (i = 0; i < this.currentPiece.left.length; i++) {
-    var row = this.currentPiece.location[0] + this.currentPiece.left[i][0];
-    var column = this.currentPiece.location[1] + this.currentPiece.left[i][1] - 1;
-    if (column < 0) {
-      return false;
-    } else if (!row < 0 && this.rows[row][column] !== "O") {
-      return false;
-    }
-  }
-  return true;
 };
 
 Board.prototype.clearCurrentPieceLocation = function() {
@@ -154,7 +112,7 @@ Board.prototype.partOfCurrentPiece = function(location) {
     var column = this.currentPiece.location[1] + this.currentPiece.occupies[i][1];
     currentSpaces.push([row, column]);
   }
-  for (i = 0; i < currentSpaces.length; i++) {
+  for (var i = 0; i < currentSpaces.length; i++) {
     if (currentSpaces[i][0] === location[0] && currentSpaces[i][1] === location[1]) {
       return true;
     }
@@ -180,7 +138,7 @@ Board.prototype.checkSpace = function(newLocation) {
 };
 
 Board.prototype.populateCurrentPiece = function() {
-  for (i = 0; i < this.currentPiece.occupies.length; i++) {
+  for (var i = 0; i < this.currentPiece.occupies.length; i++) {
     var row = this.currentPiece.location[0] + this.currentPiece.occupies[i][0];
     var column = this.currentPiece.location[1] + this.currentPiece.occupies[i][1];
     if (row >= 0) {
@@ -201,9 +159,53 @@ Board.prototype.displayBoard = function() {
 };
 
 Board.prototype.rotatePiece = function() {
-  this.clearCurrentPieceLocation();
-  this.currentPiece.rotatePiece(this);
-  this.populateCurrentPiece();
+  if (this.pieceType === "line") {
+    this.rotateLine(parentObj);
+    return;
+  } else if (this.pieceType === "square") {
+    return;
+  }
+  var newSpacesOccupied = [];
+  for (var i = 0; i < this.spacesOccupied.length; i++) {
+    switch (this.spacesOccupied[i]) {
+      case 1:
+        newSpacesOccupied.push(7)
+        break;
+      case 2:
+        newSpacesOccupied.push(4);
+        break;
+      case 3:
+        newSpacesOccupied.push(1)
+        break;
+      case 4:
+        newSpacesOccupied.push(8)
+        break;
+      case 5:
+        newSpacesOccupied.push(5)
+        break;
+      case 6:
+        newSpacesOccupied.push(2)
+        break;
+      case 7:
+        newSpacesOccupied.push(9)
+        break;
+      case 8:
+        newSpacesOccupied.push(6)
+        break;
+      case 9:
+        newSpacesOccupied.push(3)
+        break;
+    }
+  }
+  var testSpaces = [];
+  for (var i = 0; i < newSpacesOccupied.length; i++) {
+    testSpaces.push(this.possibleSpaces[newSpacesOccupied[i] - 1]);
+  }
+  if (Board.prototype.confirmClear(this.location, testSpaces, parentObj)) {
+    this.spacesOccupied = newSpacesOccupied;
+    this.setOccupies();
+    this.setBounds();
+  }
 };
 
 Board.prototype.getNewPiece = function() {
@@ -237,7 +239,7 @@ Board.prototype.getNewPiece = function() {
 
 Board.prototype.confirmClear = function(location, relativeCoordinates, parentObj) {
   /* checks to see if the given coordinates are clear. Currently only works with the edges, not with other blocks */
-  for (i = 0; i < relativeCoordinates.length; i++) {
+  for (var i = 0; i < relativeCoordinates.length; i++) {
     var row = location[0] + relativeCoordinates[i][0];
     var column = location[1] + relativeCoordinates[i][1];
     // console.log(row + " " + column);
@@ -266,7 +268,7 @@ Board.prototype.resetGame = function() {
 
 Board.prototype.checkLoseCondition = function() {
   if (this.currentPiece.location[0] < 1) {
-    for (i = 0; i < this.currentPiece.spacesOccupied.length; i++) {
+    for (var i = 0; i < this.currentPiece.spacesOccupied.length; i++) {
       var row = this.currentPiece.location[0] + this.currentPiece.occupies[i][0];
       // console.log(row);
       if (row < 0) {
@@ -318,7 +320,7 @@ Pieces.prototype.setLineBounds = function() {
 
 Pieces.prototype.setLeft = function() {
   /* Sets the coordinates for the left-most spaces occupied. Searches spaces 1-3 until it finds something, then 4-6, then 7-9 */
-  for (i = 1; i < 10;) {
+  for (var i = 1; i < 10;) {
     var j = i + 3;
     for (; i < j; i++) {
       // console.log(i);
@@ -333,7 +335,7 @@ Pieces.prototype.setLeft = function() {
 
 Pieces.prototype.setRight = function() {
   /* Sets the coordinates for the right-most spaces occupied. Searches spaces 9, 8, 7, until it finds something, then 6, 5, 4, then 3, 2, 1*/
-  for (i = 9; i > 0;) {
+  for (var i = 9; i > 0;) {
     var j = i - 3;
     for (; i > j; i--) {
       // console.log(i);
@@ -347,7 +349,7 @@ Pieces.prototype.setRight = function() {
 };
 
 Pieces.prototype.setBottom = function() {
-  for (i = 1; i < 4; i++) {
+  for (var i = 1; i < 4; i++) {
     if (this.spacesOccupied.includes(i)) {
       // debugger;
       this.bottom.push(this.possibleSpaces[i - 1]);
@@ -364,61 +366,10 @@ Pieces.prototype.setBottom = function() {
 Pieces.prototype.setOccupies = function() {
   /* takes this.spacesOccupied and converts it to an array of the proper relative coordinates */
   this.occupies = [];
-  for (i = 0; i < this.spacesOccupied.length; i++) {
+  for (var i = 0; i < this.spacesOccupied.length; i++) {
     this.occupies.push(this.possibleSpaces[this.spacesOccupied[i] - 1]);
   }
 };
-
-Pieces.prototype.rotatePiece = function(parentObj) {
-  /* rotates the pieces clockwise based on their current location in the basic 9-box grid */
-  if (this.pieceType === "line") {
-    this.rotateLine(parentObj);
-    return;
-  } else if (this.pieceType === "square") {
-    return;
-  }
-  var newSpacesOccupied = [];
-  for (i = 0; i < this.spacesOccupied.length; i++) {
-    switch (this.spacesOccupied[i]) {
-      case 1:
-        newSpacesOccupied.push(7)
-        break;
-      case 2:
-        newSpacesOccupied.push(4);
-        break;
-      case 3:
-        newSpacesOccupied.push(1)
-        break;
-      case 4:
-        newSpacesOccupied.push(8)
-        break;
-      case 5:
-        newSpacesOccupied.push(5)
-        break;
-      case 6:
-        newSpacesOccupied.push(2)
-        break;
-      case 7:
-        newSpacesOccupied.push(9)
-        break;
-      case 8:
-        newSpacesOccupied.push(6)
-        break;
-      case 9:
-        newSpacesOccupied.push(3)
-        break;
-    }
-  }
-  var testSpaces = [];
-  for (i = 0; i < newSpacesOccupied.length; i++) {
-    testSpaces.push(this.possibleSpaces[newSpacesOccupied[i] - 1]);
-  }
-  if (Board.prototype.confirmClear(this.location, testSpaces, parentObj)) {
-    this.spacesOccupied = newSpacesOccupied;
-    this.setOccupies();
-    this.setBounds();
-  }
-}
 
 Pieces.prototype.rotateLine = function(parentObj) {
   var newSpacesOccupied = []
@@ -428,7 +379,7 @@ Pieces.prototype.rotateLine = function(parentObj) {
     newSpacesOccupied.push(1, 2, 3, 10)
   }
   var testSpaces = [];
-  for (i = 0; i < newSpacesOccupied.length; i++) {
+  for (var i = 0; i < newSpacesOccupied.length; i++) {
     testSpaces.push(this.possibleSpaces[newSpacesOccupied[i] - 1]);
   }
   if (Board.prototype.confirmClear(this.location, testSpaces, parentObj)) {
@@ -503,7 +454,7 @@ Board.prototype.drawCanvas = function(context, canvas) {
   var currentColumn = 0;
   // context.rect(currentColumn, currentRow, squareWidth, squareWidth);
 
-  for (i = 0; i < 20; i++) {
+  for (var i = 0; i < 20; i++) {
     for (j = 0; j < 10; j++) {
       if (this.rows[i][j] !== "O") {
         context.fillStyle=this.rows[i][j];

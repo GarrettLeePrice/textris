1126535eee6cd0f5d1c61ac540f2b6894efc3901
Garrett Price and Nhat Hoang <garrett@>
2016-08-31 11:54:36 -0700

    add window listener to resize gameCanvas for mobile screen.

--- js/scripts.js
+++ js/scripts.js
@@ -531,7 +531,15 @@ $(document).ready(function() {
       right = false;
     }
   })
-
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $("#gameCanvas").attr("width", "250");
+      $("#gameCanvas").attr("height", "500");
+    } else {
+      $("#gameCanvas").attr("width", "350");
+      $("#gameCanvas").attr("height", "700");
+    }
+  })
   var audio = new Audio('Audio/tetris.mp3');
   $("#button1").click(function(){
     startGame();
